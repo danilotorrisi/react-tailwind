048ba10299f8184273156abe9a929e37f4f98e25
Simon-bit02 <braccio0203@gmail.com>
2021-02-15 12:35:56 +0100

    modifica tag post header e body

--- src/Post.jsx
+++ src/Post.jsx
@@ -8,14 +8,14 @@ function Post() {
     return(
         <div className="bg-black bg-opacity-80 box-content h-96 w-96  p-4 border-4 transition-shadow text-white">
             
-                <PostHeader>
+                <PostHeader
                     name="Simone Bracciolani"
                     time="17:00"
                     day="15 Febbraio 2021"
-                </PostHeader>
-                <PostBody>
+                ></PostHeader>
+                <PostBody
                     text="hello world"
-                </PostBody>
+                ></PostBody>
                 <ActionBar></ActionBar>
             </div>
         
