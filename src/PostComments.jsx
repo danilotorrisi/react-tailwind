06e29da5e51eb05e9194fdd192aa9561efcb9e07
Simon-bit02 <braccio0203@gmail.com>
2021-02-18 19:27:15 +0100

    aggiunta icona nel commento

--- src/PostComments.jsx
+++ src/PostComments.jsx
@@ -3,8 +3,8 @@ import React from "react"
 function PostComments(props) {
     return (
         <div className=" flex flex-col">
-            <div className=" flex items-center m-1">
-                <div className=" h-8">
+            <div className=" flex items-center m-1 ">
+                <div className="w-8 ">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
                 </svg>
@@ -16,9 +16,14 @@ function PostComments(props) {
                     </div>
                 </div>
             </div>
-            <div className=" flex items-start m-2 text-white text-opacity-80">
+            <div className=" flex items-start m-2 text-white text-opacity-80 ">
                 <div className="flex flex-col">
                     <div className=" flex flex-col rounded-3xl bg-gray-400 bg-opacity-20 p-2 px-4 m-2 mb-0 ml-3 shadow-sm">
+                    <div className="w-8 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
+                        </svg>
+                    </div>
                     <p className="font-medium text-white text-opacity-95 hover:underline">
                         {props.name}
                     </p>
