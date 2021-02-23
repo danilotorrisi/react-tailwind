4dfb9bb0cd9a3efa33f8c6e84f60b27838b8e745
Simon-bit02 <braccio0203@gmail.com>
2021-02-23 09:06:16 +0100

    add function IsRead

--- src/ConversationItem.jsx
+++ src/ConversationItem.jsx
@@ -1,6 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { DateTime } from 'luxon';
 
+function ClickChat() {
+  console.log('Click');
+}
+
+function ChatIsRead(props) {
+  if (props.isRead) {
+    return (
+      <div className="w-5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+        </svg>
+      </div>
+    );
+  } else {
+    return <div className="truncate w-44 text-gray-700 dark:text-white"></div>;
+  }
+}
+
 function RelativeChat(props) {
   let data = props.date;
   let dateLuxon = DateTime.fromJSDate(data);
@@ -14,7 +36,10 @@ function RelativeChat(props) {
 
 function ConversationItem(props) {
   return (
-    <div className="flex items-stretch m-1 p-2 bg-white rounded-lg hover:bg-gray-100 cursor-pointer">
+    <div
+      className="flex items-stretch m-1 p-2 bg-white rounded-lg hover:bg-gray-100 cursor-pointer"
+      onClick={() => ClickChat()}
+    >
       <img
         src={props.imgProfile}
         className="w-12 h-12 rounded-full object-cover "
@@ -26,6 +51,7 @@ function ConversationItem(props) {
             {props.lastMessage}
           </div>
           <RelativeChat {...props} />
+          <ChatIsRead {...props} />
         </div>
       </div>
     </div>
