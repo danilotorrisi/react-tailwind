075b6550f4b527a9edc3b4aa2d96727b00355b24
Simon-bit02 <braccio0203@gmail.com>
2021-02-23 10:23:05 +0100

    modifiche

--- src/ConversationItem.jsx
+++ src/ConversationItem.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { DateTime } from 'luxon';
 
-function ClickChat() {
-  console.log('Click');
-}
-
 function ChatIsRead(props) {
   if (props.isRead) {
     return (
@@ -36,13 +32,11 @@ function RelativeChat(props) {
 
 function ConversationItem(props) {
   return (
-    <div
-      className="flex items-stretch m-1 p-2 bg-white rounded-lg hover:bg-gray-100 cursor-pointer"
-      onClick={() => ClickChat()}
-    >
+    <div className="flex items-stretch m-1 p-2 bg-white rounded-lg hover:bg-gray-100 cursor-pointer">
       <img
         src={props.imgProfile}
         className="w-12 h-12 rounded-full object-cover "
+        loading="lazy"
       />
       <div className="flex flex-grow flex-col flex-shrink items-start px-3">
         <div className="font-semibold ">{props.username}</div>
